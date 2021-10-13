c0842029623faaa21c28979a74a71c74169638f3
CinTheDev <62548694+CinTheDev@users.noreply.github.com>
2021-10-13 18:14:03 +0200

    Disable multiplayer

--- BPUS/game.cpp
+++ BPUS/game.cpp
@@ -11,11 +11,14 @@ Object* obj2;
 
 class BPUS_Game : public olc::net::client_interface<GameMsg>{
 private:
-	std::unordered_map<uint32_t, sPlayerDesc> mapObjects;
+	// Networking shall be disabled for now, but the infrastructure remains
+	// It will be enabled in the future
+
+	/*std::unordered_map<uint32_t, sPlayerDesc> mapObjects;
 	uint32_t nPlayerID = 0;
 	sPlayerDesc descPlayer;
 
-	bool bWaitingForConnection = true;
+	bool bWaitingForConnection = true;*/
 
 public:
 	BPUS_Game() {
@@ -30,11 +33,35 @@ public:
 		//Obj_M::create(obj);
 		Obj_M::create(obj2);
 
-		Connect("127.0.0.1", 60000);
+		//Connect("127.0.0.1", 60000);
 	}
 
 	void update(Input* input, float dt) {
+		//listenMessages();
 
+		Obj_M::objects_update(input, dt);
+
+		// Render
+		draw_background(0x000011);
+
+		draw_rect(Vector2(300, 100), Vector2(100, 300), 0xAA0055);
+		draw_tri(Vector2(100, 100), Vector2(200, 150), Vector2(180, 250), 0x00AA55);
+		draw_cir(Vector2(300, 100), 50, 0x55AA00);
+		draw_text(Vector2(500, 200), "The Quick Brown Fox Jumps Over The Lazy Dog\nTHE QUICK BROWN FOX JUMPS OVER THE LAZY DOG\nthe quick brown fox jumps over the lazy dog", 2.5f);
+
+		draw_objects();
+
+		//sendData();
+	}
+
+	/*void sendData() {
+		olc::net::message<GameMsg> msg;
+		msg.header.id = GameMsg::Game_UpdatePlayer;
+		msg << mapObjects[nPlayerID];
+		Send(msg);
+	}
+
+	void listenMessages() {
 		if (IsConnected()) {
 			while (!Incoming().empty()) {
 				auto msg = Incoming().pop_front().msg;
@@ -87,30 +114,5 @@ public:
 				}
 			}
 		}
-
-		Obj_M::objects_update(input, dt);
-
-		// Render
-		draw_background(0x000011);
-
-		draw_rect(Vector2(300, 100), Vector2(100, 300), 0xAA0055);
-		draw_tri(Vector2(100, 100), Vector2(200, 150), Vector2(180, 250), 0x00AA55);
-		draw_cir(Vector2(300, 100), 50, 0x55AA00);
-		draw_text(Vector2(500, 200), "The Quick Brown Fox Jumps Over The Lazy Dog\nTHE QUICK BROWN FOX JUMPS OVER THE LAZY DOG\nthe quick brown fox jumps over the lazy dog", 2.5f);
-
-		// Debug
-		Vector2 v(3, 4);
-		Vector2 v_norm = v;
-		v_norm.normalize();
-		Vector2 v_fast = v.normalized();
-		std::string s = v.str() + v_norm.str() + v_fast.str();
-		draw_text(camera->position + Vector2(5, 20), s.c_str(), 2.f);
-
-		draw_objects();
-
-		olc::net::message<GameMsg> msg;
-		msg.header.id = GameMsg::Game_UpdatePlayer;
-		msg << mapObjects[nPlayerID];
-		Send(msg);
-	}
+	}*/
 };
