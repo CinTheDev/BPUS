ca858f6d91d70b7e9dff4aa8db161a7d25be4eb5
CinTheDev <62548694+CinTheDev@users.noreply.github.com>
2021-10-17 18:07:34 +0200

    Put images and objects inside game class

--- BPUS/game.cpp
+++ BPUS/game.cpp
@@ -1,15 +1,15 @@
-Image empty(0, 0, 0);
-Image test("Assets\\Images\\test2.jpg");
-Image alphatest("Assets\\Images\\test3.png");
-
-Obj::Empty* obj;
-Obj::Baseobject* obj2;
-
 class BPUS_Game{
 	// Networking has been removed for now
 	// https://www.youtube.com/watch?v=f_1lt9pfaEo has everything important
 private:
-	std::vector<Image*> images;
+#pragma region Images
+	Image empty = Image(0, 0, 0);
+	Image test = Image("Assets\\Images\\test2.jpg");
+	Image alphatest = Image("Assets\\Images\\test3.png");
+#pragma endregion
+#pragma region Objects
+	Obj::Baseobject* baseObject;
+#pragma endregion
 
 public:
 	BPUS_Game() {
@@ -17,12 +17,9 @@ public:
 		camera = new Obj::Camera(Vector2(5, 2.5), &empty, 1.f);
 		Obj_M::create(camera);
 
-		//obj = new Obj::Empty(Vector2(0, 0), &test, 200.f);
-		//obj->z = 1;
-		obj2 = new Obj::Baseobject(Vector2(7, 2.5), &alphatest, 0.5f);
-		obj2->z = 0;
-		//Obj_M::create(obj);
-		Obj_M::create(obj2);
+		baseObject = new Obj::Baseobject(Vector2(7, 2.5), &alphatest, 0.5f);
+		baseObject->z = 0;
+		Obj_M::create(baseObject);
 	}
 
 	void update(Input* input, float dt) {
@@ -32,7 +29,7 @@ public:
 		render();
 
 		// Debug
-		std::string debug = obj2->position.str() + std::to_string(obj2->size);
+		std::string debug = baseObject->position.str() + std::to_string(baseObject->size);
 		draw_text(Vector2(1, 0), debug.c_str(), .05f);
 	}
 };
