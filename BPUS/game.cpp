0574b84e474cf7686b50f2e1a0aaa83f7821f441
CinTheDev <62548694+CinTheDev@users.noreply.github.com>
2021-10-30 14:31:10 +0200

    Remove unused images

--- BPUS/game.cpp
+++ BPUS/game.cpp
@@ -2,7 +2,7 @@ class BPUS_Game{
 private:
 #pragma region Images
 	Texture popCat = Texture("Assets/Images/popcat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	Texture box = Texture("Assets/Images/test3.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	Texture box = Texture("Assets/Images/box.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 #pragma endregion
 #pragma region Objects
 	Object* cat;
