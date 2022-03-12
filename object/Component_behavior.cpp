f893bb6453bd153116eb22a1be050edcdc16fa81
Kevin Müller <cinthedev@gmail.com>
2022-03-12 20:58:13 +0100

    Keep changes, code is very spaghetti

--- object/Component_behavior.cpp
+++ object/Component_behavior.cpp
@@ -4,6 +4,8 @@ namespace comp {
     class collider : public component {
         // General class used for colliders
     public:
+        virtual Vector2* collide() { return nullptr; }
+
         void init() override {
             obj_m::registerCollider(this);
         }
@@ -13,12 +15,20 @@ namespace comp {
         }
     };
 
+    struct owned_vector2 {
+    public:
+        component* c;
+        Vector2* v;
+
+        bool operator<(const owned_vector2& vec) { return *v < *vec.v; }
+    };
+
     class collider_line : public collider {
     private:
         Vector2 normal;
 
         // First is key, second is last position
-        std::map<Vector2*, Vector2> observed_points;
+        std::map<owned_vector2, Vector2> observed_points;
 
         void getPointsInRadius(Vector2 pos, double radius) {
             std::string debug = alias;
@@ -28,35 +38,38 @@ namespace comp {
                 if (typeid(*c) != typeid(collider_line)) continue;
                 if (c->parent == parent) continue;
                 collider_line* co = (collider_line*)c;
-                Vector2* p1 = &co->point1;
+                //Vector2* p1 = &co->point1;
+                owned_vector2 v;
+                v.c = c;
+                v.v = &co->point1;
                 Vector2* p2 = &co->point2;
                 Vector2 ppos = co->parent->position;
                 
                 // If the first point is in range
-                if (((*p1 + ppos) - pos).sqrlen() < radius * radius) {
-                    if (map_contains(observed_points, p1)) continue;
+                if (((*v.v + ppos) - pos).sqrlen() < radius * radius) {
+                    if (map_contains(observed_points, v)) continue;
                     // Add it to the list if it isn't already
-                    observed_points.insert({ p1, *p1 + ppos });
+                    observed_points.insert({ v, *v.v + ppos });
                     //std::cout << alias << ": Add point 1 from component " << co->alias << std::endl;
                 }
                 // If the first point is not in range but in the list
-                else if (map_contains(observed_points, p1)) {
+                else if (map_contains(observed_points, v)) {
                     // Remove it from the list
-                    observed_points.erase(p1);
+                    observed_points.erase(v);
                     //std::cout << alias << ": Remove point 1 from component " << co->alias << std::endl;
                 }
 
                 // If the second point is in range
-                if (((*p2 + ppos) - pos).sqrlen() < radius * radius) {
-                    if (map_contains(observed_points, p2)) continue;
+                if (((*v.v + ppos) - pos).sqrlen() < radius * radius) {
+                    if (map_contains(observed_points, v)) continue;
                     // Add it to the list if it isn't already
-                    observed_points.insert({ p2, *p2 + ppos });
+                    observed_points.insert({ v, *v.v + ppos });
                     //std::cout << alias << ": Add point 2 from component " << co->alias << std::endl;
                 }
                 // If the secind point is not in range but in the list
-                else if (map_contains(observed_points, p2)) {
+                else if (map_contains(observed_points, v)) {
                     // Remove it from the list
-                    observed_points.erase(p2);
+                    observed_points.erase(v);
                     //std::cout << alias << ": Remove point 2 from component " << co->alias << std::endl;
                 }
             }
@@ -64,18 +77,7 @@ namespace comp {
 
         void update_LastPositions() {
             for (auto& elem : observed_points) {
-                elem.second = *elem.first;
-            }
-        }
-
-        void collide() {
-            //system("clear");
-            for (auto& elem : observed_points) {
-                Vector2 p1 = *elem.first - point1 - parent->position;
-                Vector2 p2 = elem.second - point1 - parent->position;
-                if (sign(normal.dot(p1) != sign(normal.dot(p2)))) {
-                    //std::cout << alias << ": Probable collision" << std::endl;
-                }
+                elem.second = *elem.first.v;
             }
         }
 
@@ -90,17 +92,34 @@ namespace comp {
 
         collider_line(Vector2 p1, Vector2 p2) : point1(p1), point2(p2) {}
 
+        Vector2* collide() override {
+            //system("clear");
+            for (auto& elem : observed_points) {
+                Vector2 p1 = *elem.first.v - point1 - parent->position;
+                Vector2 p2 = elem.second - point1 - parent->position;
+                if (sign(normal.dot(p1) != sign(normal.dot(p2)))) {
+                    //std::cout << alias << ": Probable collision" << std::endl;
+                    return new Vector2(p1);
+                }
+            }
+
+            return nullptr;
+        }
+
+        Vector2 getNormal() { return normal; }
+
         void calcNormal() {
             normal = (point2 - point1).rotate(0.5 * PI).normalized();
         }
 
         void init() override {
+            //dynamics* d = parent->getComponent<dynamics>();
+            //if (d) d->hitbox = this;
             calcNormal();
             obj_m::registerCollider(this);
         }
 
         void update(updateArguments args) override {
-            collide();
             getPointsInRadius(getMidpoint() + parent->position, (point2 - point1).len());
             update_LastPositions();
         }
@@ -133,6 +152,8 @@ namespace comp {
 
         double gravity = -9.81;
 
+        collider* hitbox;
+
         // Adds a certain amount of energy to the object (with direction)
         void addForce(Vector2 joules) {
             // using v = sqrt(2W / m)
@@ -145,11 +166,31 @@ namespace comp {
             acceleration = newtons / mass;
         }
 
+        void collide() {
+            if (!hitbox) return;
+
+            if (typeid(*hitbox) == typeid(collider_line)) {
+                collider_line* hb = (collider_line*)hitbox;
+
+                Vector2* v = hb->collide();
+
+                speed = *v - hb->getNormal() * hb->getNormal().dot(*v) * 2;
+
+                delete v;
+            }
+
+            
+        }
+
         void init() override {
             // Initialize dynamics
+            hitbox = parent->getComponent<collider>();
         }
 
         void update(updateArguments args) override {
+            // Collision
+            collide();
+
             // using v = a * t
             speed.y += gravity * args.deltatime;
             speed += acceleration * args.deltatime;
